dc6159c463f0294c065233ccd1b50c91fe51bbc2
Damien <e>
2015-12-04 10:25:06 +0000

    Addes Answer, Question and Student Class

---
+++ src/Code/Question.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Code;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Gerard
+ */
+public class Question {
+    private int questionId;
+    private Date datePosted = new Date();
+    private String tNumber;
+    private String questionText;
+    
+    public Question(){
+        setQuestionId(questionId);
+        setDatePosted(datePosted);
+        settNumber(tNumber);
+        setQuestionText(questionText);
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public String gettNumber() {
+        return tNumber;
+    }
+
+    public void settNumber(String tNumber) {
+        this.tNumber = tNumber;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+}
---
+++ src/Code/Student.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Code;
+
+/**
+ *
+ * @author t00153953
+ */
+public class Student {
+
+   
+    
+ private String Tnumber; 
+ private String Username;
+ private String UserPassword;
+ 
+ 
+ 
+ 
+    public Student(String Tnumber, String Username, String UserPassword) {
+     this.Tnumber = Tnumber;
+     this.UserPassword = UserPassword;
+     this.Username = Username;
+   
+    }
+    
+    
+     public String getTnumber() {
+        return Tnumber;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+    
+    
+     public void setTnumber(String Tnumber) {
+        this.Tnumber = Tnumber;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public void setUserPassword(String UserPassword) {
+        this.UserPassword = UserPassword;
+    }
+    
+    
+    
+    
+    }
+    
+
