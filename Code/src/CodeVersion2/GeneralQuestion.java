cba41eddbdeb496d1a750749f521d988f43bbc9a
t00136348 <l@l.l>
2015-12-07 14:39:07 +0000

    Updateed super class and toString for GeneralQuestion

--- Code/src/CodeVersion2/GeneralQuestion.java
+++ Code/src/CodeVersion2/GeneralQuestion.java
@@ -6,11 +6,14 @@ import java.util.Date;
  * Created by t00136348 on 07/12/2015.
  */
 public class GeneralQuestion extends Question{
+    //need enum for topic
+
     private Date datePosted = new Date();
 
-    public GeneralQuestion(){
+    public GeneralQuestion(String tNumber, String questionText, int questionId, Date datePosted){
         //questionId datePosted tNumber questionText;
-        super();
+        super(tNumber, questionText, questionId);
+        setDatePosted(datePosted);
     }
 
     public Date getDatePosted() {
@@ -19,5 +22,9 @@ public class GeneralQuestion extends Question{
 
     public void setDatePosted(Date datePosted) {
         this.datePosted = datePosted;
+    }
+
+    public String toString(){
+        return super.toString() + ", datePosted=" + getDatePosted();
     }
 }
