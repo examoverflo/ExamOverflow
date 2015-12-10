f2449ba7943bf95e1aa21f84d727fda83d6ba47c
Gerard <a@b.c>
2015-12-10 21:37:40 +0000

    Changed method getQuestionText in Answer class to Get Question which returns a Question object. This means the Question methods can be reused instead of creating repeated methods in the Answer class

--- CORRECT_VERSION/src/ExamOverflow_Code/Answer.java
+++ CORRECT_VERSION/src/ExamOverflow_Code/Answer.java
@@ -33,8 +33,8 @@ public class Answer {
         return dateAnswered;
     }
 
-    public String getQuestionText(){
-        return question.getQuestionText();
+    public Question getQuestion(){
+        return question;
     }
 
    @Override
