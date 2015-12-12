ca43fba511f2d5061f4ddc5306370406f2ecbe77
Damien <e>
2015-12-12 11:21:24 +0000

    Introduced new Class GeneralQuestion which is a sub class of Question

---
+++ CORRECT_VERSION/src/ExamOverflow_Code/GeneralQuestion.java
@@ -0,0 +1,26 @@
+package ExamOverflow_Code;
+
+import java.util.Date;
+
+/**
+ * Created by t00136348 on 07/12/2015.
+ */
+public class GeneralQuestion extends Question{
+
+    private Date datePosted = new Date();
+
+    public GeneralQuestion(String tNumber, String questionText, int questionId, Date datePosted, Module module){
+
+        super(questionId,questionText,datePosted, tNumber,questionText,module);
+        this.datePosted = datePosted;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+
+    public String toString(){
+        return super.toString() + "\nDate Posted: " + getDatePosted();
+    }
+}
